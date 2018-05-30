7d68cc056914069fce55dc15fa7ddd4135d8740e
Marija Marinkovic <marija.marinkovic.m@gmail.com>
2018-05-30 13:15:59 +0200

    sorting function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import List from './components/List';
 import Api from './core/api/service';
+import Filter from './components/Filter';
+import sortBy from './util/sortBy';
 import styled from 'styled-components';
+
+const DEFAULT_PER_PAGE = 4;
 
 const Placeholder = styled.div`
   max-width: 860px;
-  margin: 20px auto;
+  margin: 20px auto 60px;
 `;
+
+const sortByOptions = [
+  {prop: 'Lowest price first', val: 'price'},
+  {prop: 'Highest price first', val: 'price.reverse'},
+  {prop: 'Longest tour first', val: 'length.reverse'},
+  {prop: 'Shortest tour first', val: 'length'}
+];
 
 class App extends React.Component {
   _promise = null;
@@ -16,7 +27,8 @@ class App extends React.Component {
       isLoading: false,
       data: null,
       error: null,
-      loadedData: []
+      loadedData: [],
+      sortParam: 'price'
     }
   }
   componentDidMount() {
@@ -33,24 +45,52 @@ class App extends React.Component {
         //   }
         // })
         .then(data => this.setState({
-          data,
-          loadedData: data && data.length ? data.slice(0,4) : [],
+          ...this.handleSortData(data),
           isLoading: false
         }))
         .catch(error => this.setState({error, isLoading: false}));
     });
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.sortParam !== this.state.sortParam) {
+      this.setState({...this.handleSortData(this.state.data)});
+    }
+  }
+
   componentWillUnmount() {
     this._promise && this._promise.cancel();
   }
 
+  handleFilterSort = (e) => {
+    this.setState({sortParam: e.target.value});
+  }
+
+  handleSortData = (theData) => {
+    const { loadedData, sortParam } = this.state;
+    if (!theData || !theData.length) return {};
+
+    const totalLoaded = loadedData.length || DEFAULT_PER_PAGE;
+    const config = sortParam.split('.');
+    const data = theData.sort(sortBy(config[0], config.length > 1, parseInt));
+    return {
+      data,
+      loadedData: data.slice(0, totalLoaded)
+    };
+  }
+
   render() {
-    const { error, isLoading, loadedData } = this.state;
+    const { error, isLoading, loadedData, sortParam } = this.state;
     if (error) return <p>{error.message}</p>;
     if (isLoading) return 'Loading...';
     return (
       <Placeholder>
+        <Filter
+          label="Sort by"
+          items={sortByOptions}
+          value={sortParam}
+          onChange={this.handleFilterSort}
+        />
         <List items={loadedData} />
       </Placeholder>
     );
